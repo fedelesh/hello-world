a035d885452601386883913129af72950099e7c8
fedelesh <32619066+fedelesh@users.noreply.github.com>
2017-10-09 20:20:06 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+print("Content-type: text/html")
+print()
+print("<p><img src="http://timarkoit.com.ua/wp-content/uploads/2014/09/%D1%83%D1%80%D0%B0.png" alt="Письма мастера дзен"></p>")
