16106541c259023a5b02c414ddd5f523a577fafc
fedelesh <32619066+fedelesh@users.noreply.github.com>
2017-10-09 20:27:47 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,5 +1,2 @@
-#!/usr/bin/env python3
 
-print("Content-type: text/html")
-print()
-print("<p><img src="http://timarkoit.com.ua/wp-content/uploads/2014/09/%D1%83%D1%80%D0%B0.png" alt="Письма мастера дзен"></p>")
+print("good done")
